510b74d0515460eb94a87bdc5fd0db9ad4dea114
Lil Galo <rustyy.galo@gmail.com>
2025-10-11 13:47:21 -0300

    Initial commit

---
+++ Main.gd
@@ -0,0 +1,16 @@
+extends Node3D
+
+var mouseCaptured: bool
+
+func _ready() -> void:
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+	mouseCaptured = true
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("escape"):
+		if mouseCaptured:
+			Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+			mouseCaptured = false
+		else:
+			Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+			mouseCaptured = true
---
+++ PlayerCamera.gd
@@ -0,0 +1,28 @@
+extends Node3D
+
+@onready var CamH: Node3D = $CamH # yaw
+@onready var CamV: Node3D = $CamH/CamV # pitch
+@onready var Camera: Camera3D = $CamH/CamV/CameraArm/Camera
+
+@export var h_sens: float = 0.05
+@export var v_sens: float = 0.05
+
+var horizontal: float = 0.0
+var vertical: float = 0.0
+
+var h_acc: float = 15.0
+var v_acc: float = 15.0
+
+func _input(event: InputEvent) -> void:
+	if event is InputEventMouseMotion:
+		horizontal += -event.relative.x * h_sens
+		vertical += -event.relative.y * v_sens
+
+func _physics_process(delta: float) -> void:
+	# smooth camera
+	CamH.rotation_degrees.y = lerp(CamH.rotation_degrees.y, horizontal, h_acc * delta)
+	CamV.rotation_degrees.x = lerp(CamV.rotation_degrees.x, vertical, v_acc * delta)
+	
+	# not smooth camera
+	#CamV.rotation_degrees.x = vertical
+	#CamH.rotation_degrees.y = horizontal
